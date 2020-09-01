7ac4ee042bfc7988edee61be3d6134ef06a4302f
boostup <boostup@gmail.com>
2020-09-01 17:17:31 +0200

    first widget to be committed : facebook multilevel dropdown

---
+++ facebook-dropdown-clone/src/components/Header/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { DropdownMenu } from "../MultiLevelDropdownMenu";
+import { BellIcon, CaretIcon, MessengerIcon, PlusIcon } from "../Icons";
+import "./index.css";
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
+export default function Header() {
+  return (
+    <Navbar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu />
+      </NavItem>
+    </Navbar>
+  );
+}
---
+++ facebook-dropdown-clone/src/components/Icons/index.js
@@ -0,0 +1,8 @@
+export { ReactComponent as ArrowIcon } from "./arrow.svg";
+export { ReactComponent as BellIcon } from "./bell.svg";
+export { ReactComponent as BoltIcon } from "./bolt.svg";
+export { ReactComponent as CaretIcon } from "./caret.svg";
+export { ReactComponent as ChevronIcon } from "./chevron.svg";
+export { ReactComponent as CogIcon } from "./cog.svg";
+export { ReactComponent as MessengerIcon } from "./messenger.svg";
+export { ReactComponent as PlusIcon } from "./plus.svg";
